94e45b3d24df5cf5545c0c616243293d37bf755e
Johannes Gorset <johannes.gorset@schibsted.com>
2020-08-07 22:33:31 +0200

    Refactor index#update

--- src/index.js
+++ src/index.js
@@ -4,24 +4,28 @@ const { normalizeString, parseEnvironmentVariableAsJSON } = require('./utils');
 
 const CHANNEL_CALENDAR_MAP = parseEnvironmentVariableAsJSON('CHANNEL_CALENDAR_MAP');
 
-exports.update = async function() {
-  const map = CHANNEL_CALENDAR_MAP.map(async ({ calendarId, channelId }) => {
-    const event = await getOngoingEvent(calendarId);
-    const channel = await getChannelInfo(channelId);
+async function setTopicFromCalendar(calendarId, channelId) {
+  const event = await getOngoingEvent(calendarId);
+  const channel = await getChannelInfo(channelId);
 
-    if (event) {
-      const newTopic = event.summary;
+  if (event) {
+    const newTopic = event.summary;
 
-      if (normalizeString(newTopic) == normalizeString(channel.topic)) {
-        return `#${channel.name}: Topic already set`;
-      }
-
-      const topic = await setTopic(channelId, newTopic);
-      return `#${channel.name}: Set topic to '${newTopic}'`;
-    } else {
-      return `#${channel.name}: No ongoing event`;
+    if (normalizeString(newTopic) == normalizeString(channel.topic)) {
+      return `#${channel.name}: Topic already set`;
     }
+
+    const topic = await setTopic(channelId, newTopic);
+    return `#${channel.name}: Set topic to '${newTopic}'`;
+  } else {
+    return `#${channel.name}: No ongoing event`;
+  }
+}
+
+exports.update = async function() {
+  const promises = CHANNEL_CALENDAR_MAP.map(async ({ calendarId, channelId }) => {
+    return setTopicFromCalendar(calendarId, channelId);
   });
 
-  return Promise.all(map);
+  return Promise.all(promises);
 }
