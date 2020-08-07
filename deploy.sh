a1a49a13c8f3ff0578b44c7068b4e55a1412d270
Johannes Gorset <johannes.gorset@schibsted.com>
2020-08-07 22:05:49 +0200

    Quiet zip

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #/usr/bin/env bash
 
-zip -r function.zip .
+zip -q -r function.zip .
+
 aws lambda update-function-code \
   --function-name $AWS_LAMBDA_NAME \
   --zip-file fileb://$PWD/function.zip
